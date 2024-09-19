1b80739ba49d0eda9fc47574b57774458b689b0e
Jkoury997 <jorge_koury@me.com>
2024-09-19 11:49:15 -0300

    add

--- Apis/MakeYou/Auth/ecosystem.config.cjs
+++ Apis/MakeYou/Auth/ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
       {
-        name: 'Auth-Fichaqui-Zalei',
+        name: 'Auth-Fichaqui-MakeYou',
         script: 'bun',
         args: 'run src/app.js',
         interpreter: 'none',  // Esto le dice a PM2 que no use el intérprete de Node.js
