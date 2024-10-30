b12f15e27a538c4f8c7aa0b4dcfad2a9ade89904
Jkoury997 <jorge_koury@me.com>
2024-10-30 18:14:55 -0300

    add

--- Apis/Auth/src/routes/roleRoute.js
+++ Apis/Auth/src/routes/roleRoute.js
@@ -4,12 +4,12 @@ const roleController = require('../controllers/roleController');
 const router = express.Router();
 
 // Asignar o actualizar rol a un usuario en una empresa
-router.post('/roles/assign', roleController.assignRole);
+router.post('/assign', roleController.assignRole);
 
 // Obtener roles por userId
-router.get('/roles/user/:userId', roleController.getRolesByUserId);
+router.get('/user/:userId', roleController.getRolesByUserId);
 
 // Obtener roles por empresaId
-router.get('/roles/empresa/:empresaId', roleController.getRolesByEmpresaId);
+router.get('/empresa/:empresaId', roleController.getRolesByEmpresaId);
 
 module.exports = router;
