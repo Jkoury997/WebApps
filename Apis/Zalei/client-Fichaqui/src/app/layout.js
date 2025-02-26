8bdb53ff43b26b083ec74e700cf6efaaeaf90ae1
Jkoury997 <jorge_koury@me.com>
2025-02-25 21:39:27 -0300

    add

--- Apis/Zalei/client-Fichaqui/src/app/layout.js
+++ Apis/Zalei/client-Fichaqui/src/app/layout.js
@@ -3,20 +3,67 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const APP_NAME = "ZaleiApp Ficha Aqui";
+const APP_DEFAULT_TITLE = "My Awesome PWA App";
+const APP_TITLE_TEMPLATE = "%s - PWA App";
+const APP_DESCRIPTION = "Best PWA app in the world!";
+
 export const metadata = {
-  title: "Zalei - Fichaqui",
-  description: "App creada única y exclusivamente para fichar en Zalei Agropecuaria",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: APP_DEFAULT_TITLE,
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: APP_DEFAULT_TITLE,
+    description: APP_DESCRIPTION,
+  },
+  // Se han agregado keywords, icons y themeColor para usarlos en el head
+  keywords: ["pwa", "nextjs", "awesome app"],
+  icons: [
+    { rel: "icon", url: "/favicon.ico" },
+    { rel: "apple-touch-icon", url: "/apple-icon.png" },
+  ],
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "#FFFFFF" },
+    { media: "(prefers-color-scheme: dark)", color: "#000000" },
+  ],
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* Incluir el manifest para la PWA */}
-        <link rel="manifest" href="/manifest.json" />
-        {/* Meta tag para el color de tema */}
-        <meta name="theme-color" content="#000000" />
-        {/* Otros meta tags necesarios */}
+        {/* Incluimos el manifest y definimos los meta tags necesarios */}
+        <title>{metadata.title.default}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="manifest" href={metadata.manifest} />
+        <meta name="keywords" content={metadata.keywords.join(", ")} />
+        {metadata.themeColor.map(({ media, color }, index) => (
+          <meta key={index} name="theme-color" media={media} content={color} />
+        ))}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {metadata.icons.map(({ rel, url }, index) => (
+          <link key={index} rel={rel} href={url} />
+        ))}
+        <meta name="apple-mobile-web-app-title" content={metadata.appleWebApp.title} />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
